02cb03384f295a8dc4e3be7ee3c79efdf3f862ad
Scarlett <hwang.scarlett@gmail.com>
2019-12-22 15:54:59 +0900

    final display function added

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 /*
 Author: Scarlett Hwang
 Date: September 2019
-This Javascript file manipulates the front-end properties of a book search and recommendation webpage.
+This JS file manipulates the front-end properties of a book search and
+recommendation webpage.
 */
 
 (function() {
@@ -9,7 +10,8 @@ This Javascript file manipulates the front-end properties of a book search and r
   window.addEventListener("load", init);
   const URL = "https://www.googleapis.com/books/v1/volumes";
   const SIZE = 5;
-  let query = "";
+  let final = "";
+  let buttonCount = 0;
 
   function init() {
     id("search").addEventListener("click", function() {
@@ -38,28 +40,111 @@ This Javascript file manipulates the front-end properties of a book search and r
     }
 
     function askInterest(json) {
-      id("guide").innerText = "Do you mean...";
-      id("query-div").classList.add("hidden");
+      if (buttonCount < 3) {
+        id("guide").innerText = "What keywords interest you the most?";
+        id("query-div").classList.add("hidden");
 
-      for (let i = 0; i < SIZE; i++) {
-        console.log(json.items[i].volumeInfo.title);
-        let button = document.createElement("p");
-        let strArr = str2nounArr(json.items[i].volumeInfo.title);
-        let string = "";
-        for (let j = 0; j < strArr.length; j++) {
-          string = string + " " + strArr[j];
+        for (let i = 0; i < SIZE; i++) {
+          //console.log(json.items[i].volumeInfo.title);
+          let button = document.createElement("p");
+          let strArr = str2nounArr(json.items[i].volumeInfo.title);
+          let string = "";
+          for (let j = 0; j < strArr.length; j++) {
+            string = string + " " + strArr[j];
+          }
+          button.innerText = string;
+
+          button.addEventListener("click", function() {
+            buttonCount++;
+            final = this.innerText;
+            queryDB(this.innerText);
+          })
+
+          button.classList.add("choose-button");
+          id("book").appendChild(button);
         }
-        button.innerText = string;
-
-        button.addEventListener("click", function() {
-          queryDB(this.innerText);
-        })
-
-        button.classList.add("choose-button");
-        id("book").appendChild(button);
+      } else {
+        fetchBook(final)
       }
     }
 
+    function fetchBook(info) {
+      fetch(URL + "?q=" + info)
+       .then(checkStatus)
+       .then(JSON.parse)
+       .then(function(json) {
+         displayBook(json.items[0]);
+       })
+       .catch(function() {
+         id("error-text").classList.remove("hidden");
+         id("error-text").innerText =
+            "Something went wrong with the request. Please try again later.";
+       });
+    }
+
+    function displayBook(json) {
+      console.log(json);
+    }
+
+/* ------------------------------ Helper Functions ------------------------------ */
+
+    // Helper function that takes in a string, splice it, and returns an array of
+    // only nouns and useful words for querying
+    function str2nounArr(str) {
+      let nounArr = [];
+      let temp = str.split(" ");
+
+      for (let i = 0; i < temp.length; i++) {
+        if (!isStopWord(temp[i])) {
+          nounArr.push(temp[i].toLowerCase());
+        }
+      }
+      return nounArr;
+    }
+
+    // Helper function that takes in a word and returns a boolean value of whether
+    // the word is a noun or a userful word for querying(false) or not(true)
+    function isStopWord(word) {
+      let stopwords = ["is", "are", "the", "a", "an", "of", "in", "on", "to", "and"];
+
+      if (stopwords.includes(word.toLowerCase())) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+  /**
+   * Helper function to return the response's result text if successful, otherwise
+   * returns the rejected Promise result with an error status and corresponding text
+   * @param {object} response - response to check for success/error
+   * @returns {object} - valid result text if response was successful, otherwise rejected
+   *                     Promise result
+   */
+  function checkStatus(response) {
+    if (response.status >= 200 && response.status < 300 || response.status === 0) {
+      return response.text();
+    } else {
+      return Promise.reject(new Error(response.status + ": " + response.statusText));
+    }
+  }
+
+  /**
+   * Returns the element that has the ID attribute with the specified value.
+   * @param {string} idName - element ID
+   * @returns {object} DOM object associated with id.
+   */
+  function id(idName) {
+    return document.getElementById(idName);
+  }
+
+  function qs(className) {
+    return document.querySelector(className);
+  }
+})();
+
+
+/**----------------------------garbage codes------------------------------------
     function showBooks(json) {
       // TEST
       //console.log(json.items);
@@ -117,55 +202,4 @@ This Javascript file manipulates the front-end properties of a book search and r
         id("book").appendChild(bookDiv);
       }
     }
-
-    function str2nounArr(str) {
-      let nounArr = [];
-      let temp = str.split(" ");
-
-      for (let i = 0; i < temp.length; i++) {
-        if (!isStopWord(temp[i])) {
-          nounArr.push(temp[i].toLowerCase());
-        }
-      }
-      return nounArr;
-    }
-
-    function isStopWord(word) {
-      let stopwords = ["is", "are", "the", "a", "an", "of", "in", "on", "to", "and"];
-
-      if (stopwords.includes(word.toLowerCase())) {
-        return true;
-      } else {
-        return false;
-      }
-    }
-
-  /* ------------------------------ Helper Functions ------------------------------ */
-  /**
-   * Helper function to return the response's result text if successful, otherwise
-   * returns the rejected Promise result with an error status and corresponding text
-   * @param {object} response - response to check for success/error
-   * @returns {object} - valid result text if response was successful, otherwise rejected
-   *                     Promise result
-   */
-  function checkStatus(response) {
-    if (response.status >= 200 && response.status < 300 || response.status === 0) {
-      return response.text();
-    } else {
-      return Promise.reject(new Error(response.status + ": " + response.statusText));
-    }
-  }
-
-  /**
-   * Returns the element that has the ID attribute with the specified value.
-   * @param {string} idName - element ID
-   * @returns {object} DOM object associated with id.
-   */
-  function id(idName) {
-    return document.getElementById(idName);
-  }
-
-  function qs(className) {
-    return document.querySelector(className);
-  }
-})();
+-------------------------------------------------------------------------------**/
